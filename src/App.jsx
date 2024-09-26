7817cf92f6875bb8b0d140ef2681c6a762fc2fed
Halim Choukani <halim.choukani@gmail.com>
2024-09-26 23:26:16 +0100

    Refactor package.json and package-lock.json to add scrollmagic dependency

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,7 @@ import Highlights from "./components/Highlights";
 import Model from "./components/Model";
 import Navbar from "./components/Navbar";
 import * as Sentry from "@sentry/react";
-class ErrorBoundary extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { hasError: false };
-  }
-
-  static getDerivedStateFromError(error) {
-    return { hasError: true };
-  }
-
-  componentDidCatch(error, errorInfo) {
-    console.error("Error caught in ErrorBoundary: ", error, errorInfo);
-  }
-
-  render() {
-    if (this.state.hasError) {
-      return <h1>Something went wrong.</h1>;
-    }
-
-    return this.props.children;
-  }
-}
+import Features from "./components/Features";
 
 function App() {
   return (
@@ -33,9 +12,8 @@ function App() {
       <Navbar />
       <Hero />
       <Highlights />
-      <ErrorBoundary>
-        <Model />
-      </ErrorBoundary>
+      <Model />
+      <Features />
     </main>
   );
 }
--- src/utils/animation.js
+++ src/utils/animation.js
@@ -1,3 +1,20 @@
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
+
+export const animationGSAP = (target, animationDep, scrollDep) => {
+  gsap.to(target, {
+    ...animationDep,
+    scrollTrigger: {
+      trigger: target,
+      toggleActions: "restart reverse restart reverse",
+      start: "top 85%",
+      ...scrollDep,
+    },
+  });
+};
+
 export const animateWithGsapTimeline = (
   timeline,
   rotationRef,
