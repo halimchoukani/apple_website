2ddda14cbd9ce895b48e036e20bc2657417db692
Halim Choukani <115872704+halimchoukani@users.noreply.github.com>
2024-10-02 11:23:31 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -18,7 +18,7 @@ Sentry.init({
   ],
 
   tracesSampleRate: 1.0,
-  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  tracePropagationTargets: ["https://halimchoukani.github.io/apple_website/", /^https:\/\/yourserver\.io\/api/],
 
   replaysSessionSampleRate: 0.1,
   replaysOnErrorSampleRate: 1.0,
